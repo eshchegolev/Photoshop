27d964facd29dd5f3cf9b9346938836a4632408d
Евгений Владимирович Щёголев <shchegolev@ngs.ru>
2020-11-29 10:43:57 +0300

    Properties were added

--- Data/Pixel.cs
+++ Data/Pixel.cs
@@ -7,8 +7,33 @@ namespace MyPhotoshop
 {
     public class Pixel
     {
-        public double R;
-        public double G;
-        public double B;
+        public double Check (double value)
+        {
+            if (value < 0 || value > 1) throw new ArgumentException();
+            return value;
+        }
+
+        private double r;
+
+        public double R
+        {
+            get { return r; }
+            set { r = Check(value); }
+        }
+
+        private double g;
+
+        public double G
+        {
+            get { return g; }
+            set { g = Check(value); }
+        }
+
+        private double b;
+        public double B
+        {
+            get { return b; }
+            set { b = Check(value); }
+        }
     }
 }
